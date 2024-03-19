4bc6a9f49fa2295a20ad1620210771362d483f3d
yoon4155 <yoon415528@gmail.com>
2024-03-19 17:39:37 +0900

    추가실습파일

---
+++ src/chapter2/Constant.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// 상수와 리터럴
+		
+		// 상수선언 키워드 : fina
+		// 상수이름은 대문자로 정의한다. 규칙이 아니라 관례.
+		// 상수이름으로 값을 변경 불가능.
+		final int MAX_NUM = 100; // final 키워드를 제외하면 변수라고 부른다.
+		
+//		MAX_NUM = 200; 상수는 값을 변경하면, 에러발생
+
+	}
+
+}
